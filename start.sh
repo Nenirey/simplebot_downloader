acd724a5f7249d5b04e816bf49599a2d8fb034e8
Reinier Romero Mir <nenirey@gmail.com>
2022-04-25 17:28:26 -0400

    use defaults settings

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 python3 -m simplebot init "$ADDR" "$PASSWORD"
 python3 -m simplebot --account "$ADDR" plugin --add ./youtube.py
 python3 -m simplebot --account "$ADDR" admin --add "$ADMIN"
-python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/delay 35
-python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/max_size 304857600
-python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/part_size 10485760
+#python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/delay 30
+#python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/max_size 304857600
+#python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/part_size 10485760
 python3 -m simplebot --account "$ADDR" serve
