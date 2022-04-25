931fe67103f309d3840af79fe4c2179a035af80c
Reinier Romero Mir <nenirey@gmail.com>
2022-04-25 19:06:29 -0400

    x10

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ python3 -m simplebot init "$ADDR" "$PASSWORD"
 python3 -m simplebot --account "$ADDR" plugin --add ./youtube.py
 python3 -m simplebot --account "$ADDR" admin --add "$ADMIN"
 python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/delay 30
-#python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/max_size 304857600
-#python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/part_size 10485760
+python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/max_size 3048576000
+python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/part_size 104857600
 python3 -m simplebot --account "$ADDR" serve
