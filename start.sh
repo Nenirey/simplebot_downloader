34697b83bfccfe2188f15981ff6cc13903c66174
Reinier Romero Mir <nenirey@gmail.com>
2022-04-25 14:05:27 -0400

    add default delay

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 python3 -m simplebot init "$ADDR" "$PASSWORD"
 python3 -m simplebot --account "$ADDR" plugin --add ./youtube.py
 python3 -m simplebot --account "$ADDR" admin --add "$ADMIN"
+python3 -m simplebot --account "$ADDR" db -s simplebot_downloader/delay 30
 python3 -m simplebot --account "$ADDR" serve
